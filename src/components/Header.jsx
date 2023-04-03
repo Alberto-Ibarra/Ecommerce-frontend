4a0f33367d45a7359571f2e3cbe2f4b450f0f12a
Alberto-Ibarra <alberto.e.ibarra@gmail.com>
2023-04-03 14:16:06 -0700

    react-router-dom added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {LinkContainer} from 'react-router-bootstrap'
 import {Container, Nav, NavDropdown, Navbar} from 'react-bootstrap';
 
 
@@ -7,12 +9,18 @@ const Header = () => {
         <header>
             <Navbar bg="primary" variant='dark' expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">Ecommerce</Navbar.Brand>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand >Ecommerce</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                         <Nav>
-                            <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
-                            <Nav.Link href="/login"><i className="fas fa-user"></i>Login</Nav.Link>
+                            <LinkContainer to='/cart'>
+                                <Nav.Link><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/login'>
+                                <Nav.Link><i className="fas fa-user"></i>Login</Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
