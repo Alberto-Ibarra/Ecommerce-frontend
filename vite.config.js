515d00a0c897174128bcbe7764d8edeca4420d5a
Alberto-Ibarra <alberto.e.ibarra@gmail.com>
2023-04-14 11:14:18 -0700

    npm updated

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
+    base: './', 
     'process.env': {
       PAYPAL_CLIENT_ID: ('Ae-gufHC5Xm-C5NDM70McTGiM85r8Ya1cqLMeklz-RNrQIdeKdH32FX8RAS0Q_vrB72VQMh7SYR2fk6l'),
     },
